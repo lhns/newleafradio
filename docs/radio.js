479ed31f8ec82cd4448ef47dad9008c5601272d9
Pierre Kisters <pierrekisters@gmail.com>
2024-07-30 12:23:20 +0200

    Update radio.js

--- docs/radio.js
+++ docs/radio.js
@@ -5,24 +5,24 @@ var fadeInTimer;
 var coffeeBreakFlag = true;
 var checkWeatherFlag = false;
 var lat = 0;
-var long = 0;
+var lng = 0;
 var filePathPrefix = "Normal";
 var currentGame = "NewLeaf";
 var maxVolume = 1;
 
 async function playRadio() {
     coffeeBreakFlag = false;
-    let now = new Date();
+    const now = new Date();
 
-    let later = new Date(now);
+    const later = new Date(now);
     later.setMilliseconds(0);
     later.setSeconds(0);
     later.setMinutes(0);
-    later.setHours(now.getHours()+1);
-    console.log("Next song change: "+ later);
+    later.setHours(now.getHours() + 1);
+    console.log("Next song change: " + later);
 
-    let timeToElapse = later.getTime() - now.getTime();
-    console.log("Milliseconds before then: "+ timeToElapse);
+    const timeToElapse = later.getTime() - now.getTime();
+    console.log("Milliseconds before then: " + timeToElapse);
 
     if (checkWeatherFlag) {
         await updateCurrentWeather();
@@ -30,7 +30,7 @@ async function playRadio() {
     console.log("playing song...");
     playSong(now.getHours());
 
-    timer = setTimeout(function() {
+    timer = setTimeout(function () {
         if (!coffeeBreakFlag) {
             fadeOut();
         }
@@ -45,26 +45,28 @@ function stopRadio() {
     fadeInTimer = null;
     clearTimeout(timer);
 
-    let oldAudio = document.querySelector('audio');
-    if(oldAudio)
+    const oldAudio = document.querySelector('audio');
+    if (oldAudio) {
         oldAudio.remove();
+    }
 }
 
-function playSong(hour){
-    let prefix = filePathPrefix;
-    let suffix = (hour >= 12)? 'PM' : 'AM';
-    hour = (hour > 12)? hour - 12 : hour;
-    hour = (hour == 0)? 12 : hour;
+function playSong(hour) {
+    const prefix = filePathPrefix;
+    const suffix = (hour >= 12) ? 'PM' : 'AM';
+    hour = (hour > 12) ? hour - 12 : hour;
+    hour = (hour == 0) ? 12 : hour;
 
 
-    let oldAudio = document.querySelector('audio');
-    if(oldAudio)
+    const oldAudio = document.querySelector('audio');
+    if (oldAudio) {
         oldAudio.remove();
+    }
 
-    let player = document.createElement('audio');
+    const player = document.createElement('audio');
     player.id = `current-song`
-	player.src = `https://cdn.glitch.me/a032b7da-b36c-4292-9322-7d4c98be233b%2F${prefix}${currentGame}_${hour}${suffix}.mp3`;
-	//player.src = `songs/a032b7da-b36c-4292-9322-7d4c98be233b_${prefix}${currentGame}_${hour}${suffix}.mp3`;
+    player.src = `https://cdn.glitch.me/a032b7da-b36c-4292-9322-7d4c98be233b%2F${prefix}${currentGame}_${hour}${suffix}.mp3`;
+    //player.src = `songs/a032b7da-b36c-4292-9322-7d4c98be233b_${prefix}${currentGame}_${hour}${suffix}.mp3`;
     player.volume = 0;
     player.loop = true;
     document.body.append(player);
@@ -73,15 +75,15 @@ function playSong(hour){
     fadeIn();
 }
 
-function fadeOut(){
+function fadeOut() {
     console.log('swapping songs');
-    if (!fadeOutTimer){
-        fadeOutTimer = setInterval(function(){
+    if (!fadeOutTimer) {
+        fadeOutTimer = setInterval(function () {
             if (!coffeeBreakFlag) {
                 let currentVolume = $('#current-song')[0].volume;
-                if(currentVolume - .1 > 0)
+                if (currentVolume - .1 > 0) {
                     $('#current-song')[0].volume -= .1;
-                else{
+                } else {
                     $('#current-song')[0].volume = 0;
                     clearInterval(fadeOutTimer);
                     fadeOutTimer = null;
@@ -92,18 +94,18 @@ function fadeOut(){
     }
 }
 
-function fadeIn(){
+function fadeIn() {
     if (!fadeInTimer) {
-        fadeInTimer = setInterval(function(){
+        fadeInTimer = setInterval(function () {
             if (fadeOutTimer) {
                 clearInterval(fadeInTimer);
                 fadeInTimer = null;
             }
             if (!coffeeBreakFlag) {
-                let currentVolume = $('#current-song')[0].volume;
-                if(currentVolume + .1 < maxVolume)
+                const currentVolume = $('#current-song')[0].volume;
+                if (currentVolume + .1 < maxVolume) {
                     $('#current-song')[0].volume += .1
-                else {
+                } else {
                     $('#current-song')[0].volume = maxVolume;
                     clearInterval(fadeInTimer);
                     fadeInTimer = null;
@@ -113,8 +115,8 @@ function fadeIn(){
     }
 }
 
-function swapButtons(){
-    if ($('#start')[0].style.display == "block"){
+function swapButtons() {
+    if ($('#start')[0].style.display == "block") {
         $('#start')[0].style.display = "none";
         $('#stop')[0].style.display = "block"
     } else {
@@ -123,7 +125,7 @@ function swapButtons(){
     }
 }
 
-function weatherChanged(selected){
+function weatherChanged(selected) {
     if (selected.id == "none") {
         $('#custom-weather-sub')[0].style.display = "none";
         checkWeatherFlag = false;
@@ -141,10 +143,10 @@ function weatherChanged(selected){
     } else if (selected.id == "dynamic") {
         checkWeatherFlag = true;
         $('#custom-weather-sub')[0].style.display = "none";
-        if (navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(async function(pos) {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(async function (pos) {
                 lat = pos.coords.latitude;
-                long = pos.coords.longitude;
+                lng = pos.coords.longitude;
                 try {
                     if (!coffeeBreakFlag) {
                         playRadio();
@@ -165,11 +167,11 @@ function customWeatherRequest(selected) {
 }
 
 function setGame(selected) {
-  currentGame = selected.value;
-  console.log(currentGame);
-  if (!coffeeBreakFlag) {
+    currentGame = selected.value;
+    console.log(currentGame);
+    if (!coffeeBreakFlag) {
         playRadio();
-  }
+    }
 }
 
 function determineWeather(data) {
@@ -197,14 +199,14 @@ function determineWeather(data) {
         'blizzard'
     ];
 
-    let icon_name = data.icon.substring(data.icon.lastIndexOf('/')+1, data.icon.indexOf('?'));
-    if (icon_name.includes(',')){
-      icon_name = icon_name.substring(0, icon_name.indexOf(','));
+    let icon_name = data.icon.substring(data.icon.lastIndexOf('/') + 1, data.icon.indexOf('?'));
+    if (icon_name.includes(',')) {
+        icon_name = icon_name.substring(0, icon_name.indexOf(','));
     }
-    console.log("Weather code: "+icon_name);
-    if (snowIcons.includes(icon_name)){
+    console.log("Weather code: " + icon_name);
+    if (snowIcons.includes(icon_name)) {
         return "Snow";
-    } else if (rainIcons.includes(icon_name)){
+    } else if (rainIcons.includes(icon_name)) {
         return "Rain";
     } else {
         return "Normal";
@@ -212,28 +214,27 @@ function determineWeather(data) {
 }
 
 async function updateCurrentWeather() {
-    let pointData = await (await fetch(`https://api.weather.gov/points/${Number(lat).toFixed(4)},${Number(long).toFixed(4)}`)).json();
-    console.log("Detected County Code: "+ pointData.properties.county.substring(pointData.properties.county.lastIndexOf('/')+1, pointData.properties.county.length));
-    let forecastUrl = pointData.properties.forecastHourly
-
-    let forecast = await (await fetch(forecastUrl)).json();
-    let currentHour = forecast.properties.periods[0];
-    let precipitation = determineWeather(currentHour);
-    console.log("Interpreted Precipitaion: "+ precipitation);
+    const pointData = await (await fetch(`https://api.weather.gov/points/${Number(lat).toFixed(4)},${Number(lng).toFixed(4)}`)).json();
+    console.log("Detected County Code: " + pointData.properties.county.substring(pointData.properties.county.lastIndexOf('/') + 1, pointData.properties.county.length));
+    const forecastUrl = pointData.properties.forecastHourly
+    const forecast = await (await fetch(forecastUrl)).json();
+    const currentHour = forecast.properties.periods[0];
+    const precipitation = determineWeather(currentHour);
+    console.log("Interpreted Precipitaion: " + precipitation);
     filePathPrefix = precipitation;
 }
 
 function shrinkModal() {
     $(".modal")[0].style.display = "none";
-    let weatherOptions = $('#weather-options');
-    let currentSelection = weatherOptions.find('.active');
+    const weatherOptions = $('#weather-options');
+    const currentSelection = weatherOptions.find('.active');
     currentSelection.removeClass('active');
-    
-    let newSelection = weatherOptions.find('#none').parent();
+
+    const newSelection = weatherOptions.find('#none').parent();
     newSelection.addClass('active');
 
     filePathPrefix = 'Normal';
-    if(!coffeeBreakFlag){
+    if (!coffeeBreakFlag) {
         playRadio();
     }
 }
@@ -242,10 +243,11 @@ function showModal() {
     $(".modal")[0].style.display = "block";
 }
 
-function slideVolume(volume){
+function slideVolume(volume) {
     clearInterval(fadeInTimer);
     fadeInTimer = 0;
     maxVolume = volume.value / 100;
-    if($('#current-song')[0])
+    if ($('#current-song')[0]) {
         $('#current-song')[0].volume = maxVolume;
+    }
 }
